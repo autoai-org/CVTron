ac64d754776c8fa9854554b8d6db012c802fb833
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2018-01-30 00:50:01 +0800

    fix print prob problem

--- cvtron/utils/reporter.py
+++ cvtron/utils/reporter.py
@@ -4,8 +4,9 @@ def print_prob(prob,limit=5):
     from cvtron.data_zoo.imagenet_classes import CLASS_NAMES
     from cvtron.functions.softmax import softmax
     synset = CLASS_NAMES
+    num_classes = len(synset)
     pred = np.argsort(-prob)[::-1][:limit]
-    confidence = softmax(prob)
-    topn = [(synset[pred[0][i]], confidence[0][i]) for i in range(limit)]
+    prob = np.sort(-prob[0])
+    topn = [(synset[pred[0][i]], -prob[i]) for i in range(limit)]
     print("Top "+str(limit)+": ", topn)
     return topn
