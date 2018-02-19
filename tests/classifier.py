35375bbd13e1ec97f6ec79ddfb7ecee81c4d9290
yaoxiaozhe@email.szu.edu.cn <yaoxiaozhe@email.szu.edu.cn>
2018-02-19 22:13:44 +0800

    add coverage

--- tests/classifier.py
+++ tests/classifier.py
@@ -1,14 +1,16 @@
 from cvtron.modeling.classifier.api import *
 import unittest
-
+import math
 class TestClassifier(unittest.TestCase):
     def test_vgg(self):
         a=simple_classify_api("tests/tiger.jpeg")
-        self.assertIsInstance(a[0][0],float)
+        self.assertIsNotNone(a)
+        self.assertTrue(math.isnan(a[0][0]))
 
     def test_inception(self):
         a=simple_classify_api("tests/tiger.jpeg",'inception_v3') 
-        self.assertIsInstance(a[0][0],float)
+        self.assertIsNotNone(a)
+        self.assertTrue(math.isnan(a[0][0]))
 
 if __name__ == '__main__':
     unittest.main()
