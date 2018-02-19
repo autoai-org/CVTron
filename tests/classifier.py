b41eaeefb0d49466d1348b4c260339e300c117ae
yaoxiaozhe@email.szu.edu.cn <yaoxiaozhe@email.szu.edu.cn>
2018-02-19 22:37:28 +0800

    fix tests

--- tests/classifier.py
+++ tests/classifier.py
@@ -5,12 +5,12 @@ class TestClassifier(unittest.TestCase):
     def test_vgg(self):
         a=simple_classify_api("tests/tiger.jpeg")
         self.assertIsNotNone(a)
-        self.assertTrue(math.isnan(a[0][0]))
+        self.assertFalse(math.isnan(a[0][0]))
 
     def test_inception(self):
         a=simple_classify_api("tests/tiger.jpeg",'inception_v3') 
         self.assertIsNotNone(a)
-        self.assertTrue(math.isnan(a[0][0]))
+        self.assertFalse(math.isnan(a[0][0]))
 
 if __name__ == '__main__':
     unittest.main()
