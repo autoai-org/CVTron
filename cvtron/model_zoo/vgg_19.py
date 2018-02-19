edfb9684ef18db5ca786f1b6614193326a499bdf
yaoxiaozhe@email.szu.edu.cn <yaoxiaozhe@email.szu.edu.cn>
2018-02-19 22:36:04 +0800

    refactor vgg network

--- cvtron/model_zoo/vgg_19.py
+++ cvtron/model_zoo/vgg_19.py
@@ -26,43 +26,7 @@ def _load_image(path):
     resized_img = skimage.transform.resize(crop_img, (224, 224))
     return resized_img
 
-def VGG19(rgb):
-    """
-    Build the VGG 19 Model
-    Parameters
-    -----------
-    rgb : rgb image placeholder [batch, height, width, 3] values scaled [0, 1]
-    """
-    start_time = time.time()
-    print("build model started")
-    rgb_scaled = rgb * 255.0
-    # Convert RGB to BGR
-    if tf.__version__ <= '0.11':
-        red, green, blue = tf.split(3, 3, rgb_scaled)
-    else: # TF 1.0
-        print(rgb_scaled)
-        red, green, blue = tf.split(rgb_scaled, 3, 3)
-    if not red.get_shape().as_list()[1:] == [224, 224, 1]:
-        raise ValueError('red.get_shape().as_list()[1:] == [224, 224, 1] expected but not satisfied')
-    if not green.get_shape().as_list()[1:] == [224, 224, 1]:
-        raise ValueError('green.get_shape().as_list()[1:] == [224, 224, 1] expected but not satified')
-    if not blue.get_shape().as_list()[1:] == [224, 224, 1]:
-        raise ValueError('blue.get_shape().as_list()[1:] == [224, 224, 1] expected but not satisfied')
-    if tf.__version__ <= '0.11':
-        bgr = tf.concat(3, [
-            blue - VGG_MEAN[0],
-            green - VGG_MEAN[1],
-            red - VGG_MEAN[2],
-        ])
-    else:
-        bgr = tf.concat([
-            blue - VGG_MEAN[0],
-            green - VGG_MEAN[1],
-            red - VGG_MEAN[2],
-        ], axis=3)
-    assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
-
-    net_in = InputLayer(bgr, name='input')
+def build_network(net_in):
     network = Conv2dLayer(net_in, act = tf.nn.relu,
                 shape = [3, 3, 3, 64], strides = [1, 1, 1, 1],
                 padding='SAME', name ='conv1_1')
@@ -130,6 +94,46 @@ def VGG19(rgb):
     network = DenseLayer(network, n_units=4096, act=tf.nn.relu, name='fc6')
     network = DenseLayer(network, n_units=4096, act=tf.nn.relu, name='fc7')
     network = DenseLayer(network, n_units=1000, act=tf.identity, name='fc8')
+    return network
+
+def VGG19(rgb):
+    """
+    Build the VGG 19 Model
+    Parameters
+    -----------
+    rgb : rgb image placeholder [batch, height, width, 3] values scaled [0, 1]
+    """
+    start_time = time.time()
+    print("build model started")
+    rgb_scaled = rgb * 255.0
+    # Convert RGB to BGR
+    if tf.__version__ <= '0.11':
+        red, green, blue = tf.split(3, 3, rgb_scaled)
+    else: # TF 1.0
+        print(rgb_scaled)
+        red, green, blue = tf.split(rgb_scaled, 3, 3)
+    if not red.get_shape().as_list()[1:] == [224, 224, 1]:
+        raise ValueError('red.get_shape().as_list()[1:] == [224, 224, 1] expected but not satisfied')
+    if not green.get_shape().as_list()[1:] == [224, 224, 1]:
+        raise ValueError('green.get_shape().as_list()[1:] == [224, 224, 1] expected but not satified')
+    if not blue.get_shape().as_list()[1:] == [224, 224, 1]:
+        raise ValueError('blue.get_shape().as_list()[1:] == [224, 224, 1] expected but not satisfied')
+    if tf.__version__ <= '0.11':
+        bgr = tf.concat(3, [
+            blue - VGG_MEAN[0],
+            green - VGG_MEAN[1],
+            red - VGG_MEAN[2],
+        ])
+    else:
+        bgr = tf.concat([
+            blue - VGG_MEAN[0],
+            green - VGG_MEAN[1],
+            red - VGG_MEAN[2],
+        ], axis=3)
+    assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
+
+    net_in = InputLayer(bgr, name='input')
+    network = build_network(net_in)
     print("build model finished: %fs" % (time.time() - start_time))
     return network
 
@@ -169,56 +173,6 @@ def simple_api(rgb):
     """ input layer """
     net_in = InputLayer(bgr, name='input')
     """ conv1 """
-    network = Conv2d(net_in, n_filter=64, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv1_1')
-    network = Conv2d(network, n_filter=64, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv1_2')
-    network = MaxPool2d(network, filter_size=(2, 2), strides=(2, 2),
-                padding='SAME', name='pool1')
-    """ conv2 """
-    network = Conv2d(network, n_filter=128, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv2_1')
-    network = Conv2d(network, n_filter=128, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv2_2')
-    network = MaxPool2d(network, filter_size=(2, 2), strides=(2, 2),
-                padding='SAME', name='pool2')
-    """ conv3 """
-    network = Conv2d(network, n_filter=256, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv3_1')
-    network = Conv2d(network, n_filter=256, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv3_2')
-    network = Conv2d(network, n_filter=256, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv3_3')
-    network = Conv2d(network, n_filter=256, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv3_4')
-    network = MaxPool2d(network, filter_size=(2, 2), strides=(2, 2),
-                padding='SAME', name='pool3')
-    """ conv4 """
-    network = Conv2d(network, n_filter=512, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv4_1')
-    network = Conv2d(network, n_filter=512, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv4_2')
-    network = Conv2d(network, n_filter=512, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv4_3')
-    network = Conv2d(network, n_filter=512, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv4_4')
-    network = MaxPool2d(network, filter_size=(2, 2), strides=(2, 2),
-                padding='SAME', name='pool4')
-    """ conv5 """
-    network = Conv2d(network, n_filter=512, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv5_1')
-    network = Conv2d(network, n_filter=512, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv5_2')
-    network = Conv2d(network, n_filter=512, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv5_3')
-    network = Conv2d(network, n_filter=512, filter_size=(3, 3),
-                strides=(1, 1), act=tf.nn.relu,padding='SAME', name='conv5_4')
-    network = MaxPool2d(network, filter_size=(2, 2), strides=(2, 2),
-                padding='SAME', name='pool5')
-    """ fc 6~8 """
-    network = FlattenLayer(network, name='flatten')
-    network = DenseLayer(network, n_units=4096, act=tf.nn.relu, name='fc6')
-    network = DenseLayer(network, n_units=4096, act=tf.nn.relu, name='fc7')
-    network = DenseLayer(network, n_units=1000, act=tf.identity, name='fc8')
+    network = build_network(net_in)
     print("build model finished: %fs" % (time.time() - start_time))
     return network
