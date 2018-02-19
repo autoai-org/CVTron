90d22a2c16951e31b744c45f86ddb331286cadf1
yaoxiaozhe@email.szu.edu.cn <yaoxiaozhe@email.szu.edu.cn>
2018-02-19 16:35:28 +0800

    remove cdn to cut fees

--- cvtron/model_zoo/constant.py
+++ cvtron/model_zoo/constant.py
@@ -1,4 +1,9 @@
 VGG_MEAN = [103.939, 116.779, 123.68]
+'''
 INCEPTION_CKPT_URL = 'https://deepbase-1251068397.cos.ap-guangzhou.myqcloud.com/inception_v3.ckpt'
 VGG_NPY_URL = 'https://deepbase-1251068397.cos.ap-guangzhou.myqcloud.com/vgg19.npy'
 LAPSRN_URL = 'https://deepbase-1251068397.cos.ap-guangzhou.myqcloud.com/lapsrn.npz'
+'''
+INCEPTION_CKPT_URL = 'http://temp.zhitantech.com/static/inception_v3.ckpt'
+VGG_NPY_URL = 'http://temp.zhitantech.com/static/vgg19.npy'
+LAPSRN_URL = 'http://temp.zhitantech.com/static/lapsrn.npz'
