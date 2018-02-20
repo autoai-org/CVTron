6fa7a1c74b32787292f2b296dd8cc8073c3b0cc9
yaoxiaozhe@email.szu.edu.cn <yaoxiaozhe@email.szu.edu.cn>
2018-02-21 00:08:23 +0800

    add trainer test

--- tests/classifier_train.py
+++ tests/classifier_train.py
@@ -1,2 +1,13 @@
+#coding:utf-8
+import os
 from cvtron.trainers.classifier_trainer import ClassifierTrainer
-
+from cvtron.utils.config_loader import MODEL_ZOO_PATH
+trainer = ClassifierTrainer(batch_size=64, 
+                            epochs=100, 
+                            learning_rate=1e-3,
+                            log_dir='./logs',
+                            dataset_dir='./cvtron/data_zoo/hico/tfrecords/train', 
+                            checkpoint=MODEL_ZOO_PATH, 
+                            num_classes=600, 
+                            is_fine_tune=True)
+trainer.bootstrap()
