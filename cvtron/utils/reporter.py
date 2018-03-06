ea598478302a4c592d1dbb76c712b9c8d89ee997
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2018-03-06 19:32:38 +0800

    fix hardware reporter

--- cvtron/utils/reporter.py
+++ cvtron/utils/reporter.py
@@ -1,6 +1,7 @@
-#coding:utf-8
+# coding:utf-8
 import numpy as np
 import tensorflow as tf
+
 
 def print_prob(prob, limit=5):
     from cvtron.data_zoo.imagenet_classes import CLASS_NAMES
@@ -12,6 +13,7 @@ def print_prob(prob, limit=5):
     print("Top "+str(limit)+": ", topn)
     return topn
 
+
 def print_detect_result(result):
     print('xmin:'+str(result['xmin']))
     print('ymin:'+str(result['ymin']))
@@ -20,17 +22,27 @@ def print_detect_result(result):
     print('class_num'+str(result['class_num']))
     return result
 
+
 def report_hardware():
     from tensorflow.python.client import device_lib
     local_device_protos = device_lib.list_local_devices()
     result = {
-        'cpu':[],
-        'gpu':[]
+        'cpu': [],
+        'gpu': []
     }
     for each in local_device_protos:
         if each.device_type == 'CPU':
             result['cpu'].append(each.name)
         else:
-            result['gpu'].append(each.physical_device_desc)
+            descs = each.physical_device_desc.split(',')
+            device = descs[0].split(':')[1].replace(' ', '')
+            name = descs[1].split(':')[1].replace(' ', '')
+            compute_capability = descs[3].split(':')[1].replace(' ', '')
+            device_obj = {
+                'device': device,
+                'name': name,
+                'compute_capability': compute_capability
+            }
+            result['gpu'].append(device_obj)
     print(result)
-    return local_device_protos
+    return result
