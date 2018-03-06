13b0fcf53c2e601457b888262e38d00b4d845315
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2018-03-06 16:28:31 +0800

    gpu report

--- cvtron/utils/reporter.py
+++ cvtron/utils/reporter.py
@@ -1,5 +1,6 @@
+#coding:utf-8
 import numpy as np
-
+import tensorflow as tf
 
 def print_prob(prob, limit=5):
     from cvtron.data_zoo.imagenet_classes import CLASS_NAMES
@@ -17,4 +18,14 @@ def print_detect_result(result):
     print('xmax:'+str(result['xmax']))
     print('ymax:'+str(result['ymax']))
     print('class_num'+str(result['class_num']))
+    return result
+
+def report_hardware():
+    from tensorflow.python.client import device_lib
+    local_device_protos = device_lib.list_local_devices()
+    devices = local_device_protos
+    result = {
+        'gpu':devices[1].physical_device_desc
+    }
+    print(result)
     return result
