14dfdfaae2d068522f347e2cf6d4fec971020ad4
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2018-03-16 00:32:20 +0800

    add resnet

---
+++ cvtron/model_zoo/deeplab/deeplabV3.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+import tensorflow as tf
+
+slim = tf.contrib.slim
+
+from cvtron.data_zoo.imagenet_mean import _R_MEAN
+from cvtron.data_zoo.imagenet_mean import _B_MEAN
+from cvtron.data_zoo.imagenet_mean import _G_MEAN
+
+
