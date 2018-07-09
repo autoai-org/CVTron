3f21b13e2493c33fcac668d0a81fa57b3735748b
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2018-07-09 13:13:09 +0800

    [wip] object detection install api

--- cvtron/3rdparty/object_detection/install-ubuntu.sh
+++ cvtron/3rdparty/object_detection/install-ubuntu.sh
@@ -2,6 +2,12 @@
 read -r -p"This script will install Object Detection API, Are you sure? [Y/n]" response
 response=${response,,}
 if [[ $response =~ ^(yes|y| ) ]]; then
+    echo 'git clone https://github.com/tensorflow/models.git'
+    git clone https://github.com/tensorflow/models.git tmp/
+    cp -rf tmp/.git object_detection/
+    cd object_detection
+    git checkout -f master
+    rm -rf tmp
     echo 'protoc object_detection/protos/*.proto --python_out=.'
     protoc object_detection/protos/*.proto --python_out=.
 fi
